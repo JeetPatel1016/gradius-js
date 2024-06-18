b987e24ef332d7ca845834c49c5218beb37fb61c
Jeet Patel <Jeetpatel1011@gmail.com>
2024-06-18 09:22:06 -0400

    Fixed base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/gradius-js", // Ensure this matches your repository name
+  base: "/gradius-js/", // Ensure this matches your repository name
   build: {
     outDir: "dist", // Ensure this matches the publish_dir in your workflow
   },
