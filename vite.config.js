d5f0e14fce65652df5bd52838d6e8b3cf1ff6227
Jeet Patel <Jeetpatel1011@gmail.com>
2024-06-18 09:14:57 -0400

    Added vite config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/gradius-js", // Ensure this matches your repository name
+  build: {
+    outDir: "dist", // Ensure this matches the publish_dir in your workflow
+  },
+});
